8aa174df75693fefa0e44259a721c2b61e993a4c
Jeoee-Xv <70570785+Jeoee-Xv@users.noreply.github.com>
2021-10-15 14:26:53 +0800

    Add files via upload

---
+++ Top_10_days.js
@@ -0,0 +1,4 @@
+var vg_1 = "Top_10_days.vg.json";
+vegaEmbed("#Top_10_days", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
---
+++ Top_20_fires.js
@@ -0,0 +1,4 @@
+var vg_1 = "Top_20_fires.vg.json";
+vegaEmbed("#Top_20_fires", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
